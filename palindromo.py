c443714ce17e52adcfa0f3d18633c1541fed52e1
Daniel Montero <monterodanieleduardo@gmail.com>
2020-11-08 13:57:45 -0500

    Validación palíndromo e impresión true/false

--- palindromo.py
+++ palindromo.py
@@ -1,4 +1,8 @@
 palabra = input("Por favor ingrese una palabra: ")
 
+palabra = palabra.replace(" ","")
 
-
+if palabra[::-1] == palabra[::1]:
+    print("Es un Palíndromo")
+else:
+    print("No es un Palíndromo")
